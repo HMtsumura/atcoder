fa0d53f7c734c48b358beaaa26a9067c2c88ba86
HMtsumura <hironori0811@gmail.com>
2024-05-06 19:40:09 +0900

    b問題コミット

--- BeginnerContest/347/b.go
+++ BeginnerContest/347/b.go
@@ -1,20 +1,19 @@
 package main
 
-import {
-	"fmt"	
-}
-
+import (
+	"fmt"
+)
 
 func main() {
 	var S string
 	fmt.Scan(&S)
-	result := make([]string, 1, 2)
+	result := make(map[string]int)
 	for i := 0; i < len(S); i++ {
-		for j := i; j < len(S[i:]); j++ {
-			if !	slices.Contains(result, S[i:j]) {
-				result.append(S[i:j])
+		for j := i; j < len(S); j++ {
+			if result[S[i:j+1]] != 1 {
+				result[S[i:j+1]] = 1
 			}
 		}
 	}
-	fmt.Print((result))
+	fmt.Print(len(result))
 }
